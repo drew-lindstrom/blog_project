be41cd52339714a05cb4d17dcb42d7673cc5b202
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-08-02 13:39:24 -0500

    changed to django-heroku

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_on_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -157,4 +157,4 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-django_on_heroku.settings(locals())
+django_heroku.settings(locals())
